5f0d6640f7a2353f4eb1db7a398f760d303bf4b3
cimu34148 <1366061829@qq.com>
2020-04-30 14:06:58 +0800

    1

--- app/service/accessToken.js
+++ app/service/accessToken.js
@@ -3,14 +3,9 @@ const Service = require('egg').Service;
 class AccessTokenService extends Service {
     async getFromWechat() {
         const { wechatBaseUrl, wechatAppId, wechatAppSecret } = this.config;
-        // const { data: {access_token} } = await this.ctx.curl(`${wechatBaseUrl}token?grant_type=client_credential&appid=${wechatAppId}&secret=${wechatAppSecret}`, {
-        //     dataType: 'json'
-        // });
-        // console.log(access_token)
-        const result = await this.ctx.curl(`${wechatBaseUrl}token?grant_type=client_credential&appid=${wechatAppId}&secret=${wechatAppSecret}`, {
+        const { data: {access_token} } = await this.ctx.curl(`${wechatBaseUrl}token?grant_type=client_credential&appid=${wechatAppId}&secret=${wechatAppSecret}`, {
             dataType: 'json'
         });
-        console.log(result)
         await this.app.redis.set('accessToken', access_token, 'Ex', 7200);
         return access_token;
     }
